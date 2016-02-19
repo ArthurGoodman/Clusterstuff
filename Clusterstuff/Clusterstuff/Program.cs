b00437ce539d9ac435ee408c4471684e57d418df
Arthur Goodman <arthur.goodman3@gmail.com>
2016-02-19 21:18:18 +0200

    first

---
+++ Clusterstuff/Clusterstuff/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Iris {
+    class Program {
+        static void Main(string[] args) {
+            string[] lines = File.ReadAllLines("iris.dat");
+
+            double[][] data = new double[lines.Length][];
+
+            int i = 0;
+
+            foreach (string line in lines) {
+                string[] values = line.Split(',');
+                data[i++] = values.Take(4).Select(v => double.Parse(v)).ToArray();
+            }
+        }
+    }
+}
